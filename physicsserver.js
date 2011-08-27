e7f89465a472effe0b5366feecca16b3d780cf5d
Matthew Sorensen <scholl.ma@gmail.com>
2011-08-26 21:53:19 -0700

    spawning works, but must be done manually - /root/physics.sh

--- physicsserver.js
+++ physicsserver.js
@@ -1,18 +1,18 @@
-var http = require('http');
+var serverNumber = parseInt(process.argv[2]),
+    port = [1337,1338,1339][serverNumber];
 
+
+var http = require('http');
 var server =  http.createServer(function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/plain'});
 	res.end('Hello World\n');
     });
 
-server.listen(parseInt(process.argv[2]),function(){
+server.listen(port,function(){
         if (process.getuid() === 0)
             require('fs').stat(__filename, function(err, stats) {
                     if (err) return console.log(err)
                                  process.setuid(stats.uid);
 		});
-	console.log("Started physics slave n");
+	console.log("Started physics server "+serverNumber);
     });
-
-
-
