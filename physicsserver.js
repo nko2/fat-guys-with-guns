c75e542a05054ca5a4ca83c88f4583a536db71a4
Matthew Sorensen <scholl.ma@gmail.com>
2011-08-27 11:24:29 -0700

    Trying to make progress. Needs logic for choosing controllers and all sorts of stuff

--- physicsserver.js
+++ physicsserver.js
@@ -3,17 +3,24 @@ var serverNumber = parseInt(process.argv[2]),
 // Downgrade from root
 if (process.getuid() === 0)
     require('fs').stat(__filename, function(err, stats) {
-	    if (err) return console.log(err)
-			 process.setuid(stats.uid);
+	    if (err) 
+		return console.log(err);
+	    process.setuid(stats.uid);
 	});
 console.log("Started physics server "+serverNumber);
-var io = require('socket.io').listen(port);
+var io = require('socket.io').listen(port),
+    redis = require('redis').createClient();
+
+redis.on("error", function (err) {
+	console.log("Redis connection error to " + redis.host + ":" + redis.port + " - " + err);
+    });
 
 var games = {},
     gameBySocketId = {},
-    gamesLeft = 4;//Random guess - can we simulate 4 games per socket?
+    redisKeys = {},
+    gamesLeft = 4;//Random guess - can we simulate 4 games per process?
 io.sockets.on('connection',function(socket){
-	socket.on('subscribe',function(type,gameId){
+	socket.on('subscribe',function(type,gameId,redisKey){
 		var room = games[gameId];
 		if(typeof room === 'undefined'){
 		    if(gamesLeft > 0){
@@ -27,6 +34,7 @@ io.sockets.on('connection',function(socket){
 		if(room)
 		    if(room.join(socket,type)){
 			gameBySocketId[socket.id] = gameId;
+			redisKeys[socket.id] = redisKey;
 			//Probably want to send current world data here
 		    }else{
 			socket.emit('error',"This game room is full");			
@@ -44,16 +52,17 @@ io.sockets.on('connection',function(socket){
 		    }
 		    delete gameBySocketId[socket.id];
 		}		
+		delete redisKeys[socket.id];
 	    });
     });
-
 Game = function(id){
     this.gameId = id;
     //Initialize a new physics object here
     this.physics = null;
+    this.c0 = null;
+    this.c1 = null;
     this.viewers = 0;
     this.controllers = 0;
-
     this.all_sockets = id+'-all';
     this.controller_sockets = id+'-control';
 };
@@ -79,10 +88,32 @@ Game.prototype.empty = function(){
 };
 Game.prototype.remove = function(id){
     var controller_room = io.sockets.manager.rooms[this.controller_sockets] || [];
-    if(-1 != controller_room.indexOf(id))
+    if(-1 != controller_room.indexOf(id)){
+	// If this is c1 or c0, the other one wins.
+	if(id == this.c0){
+	    this.c0 = null;
+	    this.end(1);
+	}else if(id == this.c1){
+	    this.c1 = null;
+	    this.end(0);
+	}
 	this.controllers--;
-    else
+    }else
 	this.viewers--;
 };
-
-
+Game.prototype.end = function(winner){
+    // for both controller sockets (if they exist) start ignoring the paddle data
+    if(this.c0) io.sockets.socket(this.c0).removeAllListeners('paddle');
+    if(this.c1) io.sockets.socket(this.c1).removeAllListeners('paddle');
+    winner = [this.c0,this.c1][winner];
+    redis.get(redisKeys[winner],function(err,data){
+	    var fail = {};
+	    io.sockets.in(this.all_sockets).emit('win',data || fail);
+	    this.begin();//Start over...
+	});
+};
+Game.prototype.begin = function(){
+    //Something... Something... Some function to derive new controllers - fortunately we have redis data to make this awesome
+    // Then start the sequence of events - tell the two controllers to start - this should probably have a count-down?
+    // trigger everyone else to reset, and reset the physics simulation.
+};
