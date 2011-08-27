fe641d33b14c4a795709cd1ae222453391513a1f
Jason Malcolm <jaciones@hotmail.com>
2011-08-27 03:27:28 -0700

    Tweaks for UI

--- controllers/index.js
+++ controllers/index.js
@@ -8,15 +8,18 @@ module.exports = function(app, redis_client) {
 
     app.post('/register', function(req, res) {
         var user_name = req.body.user_name;
-        var phone_secret = StringHelper.createRandomWord(6);
 
-        redis_client.hmset("phone_secret:"+phone_secret, {
-            "user_name" : user_name
+        findUniqueKey(function(phone_secret) {
+            redis_client.hmset("phone_secret:"+phone_secret, {
+                "user_name" : user_name
+            });
+            // Expire ids so we can reuse them later
+            redis_client.expire("phone_secret:"+phone_secret, 86400); // One Day
+
+            res.cookie('user_id', user_name, { httpOnly: false });
+            res.cookie('phone_secret', phone_secret, { httpOnly: false });
+            res.redirect('/home');
         });
-
-        res.cookie('user_id', user_name, { httpOnly: false });
-        res.cookie('phone_secret', phone_secret, { httpOnly: false });
-        res.redirect('/home');
     });
 
     app.get('/home', function(req, res) {
@@ -38,4 +41,19 @@ module.exports = function(app, redis_client) {
             phone_secret : phone_secret
         });
     });
+
+    function findUniqueKey(callback) {
+        var phone_secret = StringHelper.createRandomWord(7);
+        // Make sure we don't already have a redis object with this value, if we do, get another
+        redis_client.hgetall("phone_secret:"+phone_secret, function(err, data) {
+            if (err) {
+                console.warn(err);
+            }
+            if (!data.user_name) {
+                callback(phone_secret)
+            }else {
+                findUniqueKey(callback);
+            }
+        });
+    }
 }
