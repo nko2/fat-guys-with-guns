4b83e1fbf11041ee9944f3a7fcb080df296c87a7
Kristofor Selden <kris@intersect.com>
2011-08-28 15:45:02 -0700

    update practice

---
+++ public/javascripts/pratice_observe.js
@@ -0,0 +1,30 @@
+$(function () {
+  var g = require().GameDef,
+      gameLogic = new GameLogic(g),
+      gameView = new GameView(g, $('#game_view'));
+
+  gameView.drawState(gameLogic.getState());
+  connectToRoom(port,id)
+
+  setInterval(function() {
+    var state = gameLogic.step(1/60);
+          gameView.drawState(state);
+    }, 1000/60
+  );
+
+  function onPaddle(index, points) {
+    gameLogic.setTouchPoints(index, points);
+  }
+
+  function onDbl(index) {
+    gameLogic.doubleTouch(index);
+  }
+
+  function connectToRoom(port,id){
+    var socket = io.connect(['http://',document.domain,':',port].join(''));
+    socket.on('paddle', onPaddle);
+    socket.on('dbl', onDbl);
+    socket.emit('subscribe','viewer',id);
+    return socket;
+  }
+});
