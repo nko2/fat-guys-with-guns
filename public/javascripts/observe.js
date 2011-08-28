11ef0f53d3ad6ea2afcc26fc3151d93b1e4fda50
Matthew Sorensen <scholl.ma@gmail.com>
2011-08-28 16:02:58 -0700

    log messages

--- public/javascripts/observe.js
+++ public/javascripts/observe.js
@@ -1,36 +1,39 @@
 $(function () {
   var gameView = new GameView(require().GameDef, $('#game_view'));
   connectToRoom(port,room_id,phone_secret);
+  var $mess = $('.message');
+  function emitMessage(m){$mess.text(m);console.log(m);}
 
   function onGameOver(winner){
-    console.log("Game over, won by"+winner);
+    console.log(winner);
+    console.log("Game over, won by"+winner.user_name);
   }
   function onCountdown(n){
-      console.log(n+' seconds remaining');
+      if(n > 0){
+	  emitMessage(n.toString()+' seconds');
+	  setTimeout(function(){onCountdown(n-1);},1000);
+      }else{
+	  emitMessage('');
+      }
   }
   function onUpdate(dat){
     gameView.drawState(dat);
   }
   function onStart(gameState){
-    console.log("Starting game");
-  }
-
-  function onState(currentState){
-
-  }
-  function onError(err){
-    console.log('Damn error'+err);
+      emitMessage("Game started!");
+      setTimeout(function(){emitMessage('');},2000);
   }
 
   function connectToRoom(port,room,redis){
     var socket = io.connect(['http://',document.domain,':',port].join(''));
-    socket.on('error',onError);
+    socket.on('error',emitMessage);
     socket.on('win',onGameOver);
     socket.on('time',onCountdown);
     socket.on('start',onStart);
-    socket.on('state',onState);
+    socket.on('state',onUpdate);
     socket.on('update',onUpdate);
     socket.emit('subscribe','viewer',room,redis);
     return socket;
   }
 });
+
