ffae8b0054af4b5d5dcdfc7e7df0c96edbbd5804
Kristofor Selden <kris@intersect.com>
2011-08-27 20:01:39 -0700

    add curved roof

--- public/javascripts/game_logic.js
+++ public/javascripts/game_logic.js
@@ -47,7 +47,7 @@ GameLogic.prototype.reset = function() {
 };
 
 GameLogic.prototype.buildStaticBodies = function() {
-  var fixDef = new Box2D.Dynamics.b2FixtureDef, bodyDef = new Box2D.Dynamics.b2BodyDef, wallWidth = 60;
+  var fixDef = new Box2D.Dynamics.b2FixtureDef, bodyDef = new Box2D.Dynamics.b2BodyDef;
   bodyDef.type = Box2D.Dynamics.b2Body.b2_staticBody;
   fixDef.density = 1.0;
   fixDef.friction = 0.5;
@@ -60,6 +60,7 @@ GameLogic.prototype.buildStaticBodies = function() {
   var halfHeight = height / 2;
   var netHalfWidth = this.gameDef.net.width / s / 2;
   var netHalfHeight = this.gameDef.net.height / s / 2;
+  var radius = this.gameDef.radius / s;
 
 
   // left
@@ -74,12 +75,6 @@ GameLogic.prototype.buildStaticBodies = function() {
   bodyDef.position.Set(width+1, halfHeight);
   this.world.CreateBody(bodyDef).CreateFixture(fixDef);
 
-  // top
-  fixDef.shape = new Box2D.Collision.Shapes.b2PolygonShape;
-  fixDef.shape.SetAsBox(halfWidth+1, 1);
-  bodyDef.position.Set(halfWidth, -1);
-  this.world.CreateBody(bodyDef).CreateFixture(fixDef);
-
   // floor
   fixDef.shape = new Box2D.Collision.Shapes.b2PolygonShape;
   fixDef.shape.SetAsBox(halfWidth+1, 1);
@@ -91,4 +86,22 @@ GameLogic.prototype.buildStaticBodies = function() {
   fixDef.shape.SetAsBox(netHalfWidth, netHalfHeight);
   bodyDef.position.Set(halfWidth, height - netHalfHeight);
   this.world.CreateBody(bodyDef).CreateFixture(fixDef);
+
+  // Math.acos(4.0/5.0) = 0.6435011087932844
+  // Math.PI/2 = 1.5707963267948966
+  // (1.5707963267948966 - 0.6435011087932843) = 0.9272952180016123
+  var step = 2 * 0.9272952180016123 / 5;
+
+  for (var i=0; i<6; i++) {
+    var angle = 0.6435011087932844 + step * i;
+    var cos = Math.cos(angle);
+    var sin = Math.sin(angle);
+    var x = cos * (radius+1);
+    var y = sin * (radius+1);
+    fixDef.shape = new Box2D.Collision.Shapes.b2PolygonShape;
+    fixDef.shape.SetAsBox(0.30909840600053745 * (radius+1), 1);
+    bodyDef.position.Set(halfWidth + x, height - y);
+    bodyDef.angle = angle * -1 + 1.5707963267948966;
+    this.world.CreateBody(bodyDef).CreateFixture(fixDef);
+  }
 };
