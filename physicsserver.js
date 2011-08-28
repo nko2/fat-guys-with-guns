1423728e81d801984873c88bc3fb04cc3014b21b
Matthew Sorensen <scholl.ma@gmail.com>
2011-08-28 11:07:54 -0700

    added dbl event to server

--- physicsserver.js
+++ physicsserver.js
@@ -3,10 +3,10 @@ var serverNumber = parseInt(process.argv[2]),
 // Downgrade from root
 if (process.getuid() === 0)
     require('fs').stat(__filename, function(err, stats) {
-	    if (err)
-		return console.log(err);
-	    process.setuid(stats.uid);
-	});
+            if (err)
+                return console.log(err);
+            process.setuid(stats.uid);
+        });
 console.log("Started physics server "+serverNumber);
 var io = require('socket.io').listen(port),
     redis = require('redis').createClient(),
@@ -14,7 +14,7 @@ var io = require('socket.io').listen(port),
     GameDef = require('./public/javascripts/game_def.js').GameDef;
 
 redis.on("error", function (err) {
-	console.log("Redis connection error to " + redis.host + ":" + redis.port + " - " + err);
+        console.log("Redis connection error to " + redis.host + ":" + redis.port + " - " + err);
     });
 
 var games = {},
@@ -23,30 +23,30 @@ var games = {},
     gamesPerProcess = 4;
 
 io.sockets.on('connection',function(socket){
-	socket.on('subscribe',function(type,gameId,redisKey){
-		var room = games[gameId];
-		if(typeof room === 'undefined'){
-		    socket.emit('error',"Bad room id");
-		}
-		if(room)
-		    if(room.join(socket,type)){
-			gameBySocketId[socket.id] = gameId;
-			redisKeys[socket.id] = redisKey;
-			socket.emit('update',room.physics.getState());
-		    }else{
-			socket.emit('error',"This game room is full");
-		    }
-	    });
-	socket.on('disconnect',function(){
-		var gameId, game;
-		if(gameId = gameBySocketId[socket.id]){
-		    if(game=games[gameId]){
-			game.remove(socket.id);
-		    }
-		    delete gameBySocketId[socket.id];
-		}
-		delete redisKeys[socket.id];
-	    });
+        socket.on('subscribe',function(type,gameId,redisKey){
+                var room = games[gameId];
+                if(typeof room === 'undefined'){
+                    socket.emit('error',"Bad room id");
+                }
+                if(room)
+                    if(room.join(socket,type)){
+                        gameBySocketId[socket.id] = gameId;
+                        redisKeys[socket.id] = redisKey;
+                        socket.emit('update',room.physics.getState());
+                    }else{
+                        socket.emit('error',"This game room is full");
+                    }
+            });
+        socket.on('disconnect',function(){
+                var gameId, game;
+                if(gameId = gameBySocketId[socket.id]){
+                    if(game=games[gameId]){
+                        game.remove(socket.id);
+                    }
+                    delete gameBySocketId[socket.id];
+                }
+                delete redisKeys[socket.id];
+            });
     });
 Game = function(id){
     this.gameId = id;
@@ -62,19 +62,19 @@ Game = function(id){
 };
 Game.prototype.join = function(socket,type){
     if(type == 'viewer'){
-	//Probably can manage 4 viewers and 4 controllers?
-	if(this.viewers < 4){
-	    socket.join(this.all_sockets);
-	    this.viewers++;
-	    return true;
-	}
+        //Probably can manage 4 viewers and 4 controllers?
+        if(this.viewers < 4){
+            socket.join(this.all_sockets);
+            this.viewers++;
+            return true;
+        }
     }else if(type == 'controller'){
-	if(this.controllers < 4){
-	    socket.join(this.all_sockets);
-	    socket.join(this.controller_sockets);
-	    this.controllers++;
-	    return true;
-	}
+        if(this.controllers < 4){
+            socket.join(this.all_sockets);
+            socket.join(this.controller_sockets);
+            this.controllers++;
+            return true;
+        }
     }
     return false;
 };
@@ -84,56 +84,70 @@ Game.prototype.empty = function(){
 Game.prototype.remove = function(id){
     var controller_room = io.sockets.manager.rooms['/'+this.controller_sockets] || [];
     if(-1 != controller_room.indexOf(id+'')){
-	// If this is c1 or c0, the other one wins.
-	if(id == this.c0){
-	    this.c0 = null;
-	    this.end(1);
-	}else if(id == this.c1){
-	    this.c1 = null;
-	    this.end(0);
-	}
-	this.controllers--;
+        // If this is c1 or c0, the other one wins.
+        if(id == this.c0){
+            this.c0 = null;
+            this.end(1);
+        }else if(id == this.c1){
+            this.c1 = null;
+            this.end(0);
+        }
+        this.controllers--;
     }else
-	this.viewers--;
+        this.viewers--;
 };
 Game.prototype.end = function(winner){
     // for both controller sockets (if they exist) start ignoring the paddle data
     this.running = false;
-    if(this.c0) io.sockets.socket(this.c0).removeAllListeners('paddle');
-    if(this.c1) io.sockets.socket(this.c1).removeAllListeners('paddle');
+    if(this.c0){
+        var s = io.sockets.socket(this.c0);
+        s.removeAllListeners('paddle');
+        s.removeAllListeners('dbl');
+    }
+    if(this.c1){
+        var s = io.sockets.socket(this.c1);
+        s.removeAllListeners('paddle');
+        s.removeAllListeners('dbl');
+    }
     winner = [this.c0,this.c1][winner];
     (function(game){
-	redis.get(redisKeys[winner],function(err,data){
-		var fail = {};
-		io.sockets.in(game.all_sockets).emit('win',data || fail);
-	    });
+        redis.get(redisKeys[winner],function(err,data){
+                var fail = {};
+                io.sockets.in(game.all_sockets).emit('win',data || fail);
+            });
     })(this);
 };
 Game.prototype.begin = function(){
     var controllers = io.sockets.manager.rooms['/'+this.controller_sockets];
     (function(game){
-	chooseControllers(controllers,function(a,b){
-		game.c0 = a;
-		game.c1 = b;
-		console.log(a);
-		console.log(b);
-		io.sockets.socket(game.c0).emit('nominate',0);
-		io.sockets.socket(game.c1).emit('nominate',1);
-		io.sockets.in(game.all_sockets).emit('time',15);
-		setTimeout(function(){
-			if(game.running){
-			    game.physics.reset();
-			    io.sockets.in(game.all_sockets).emit('start',game.physics.getState());
-			    io.sockets.socket(game.c0).on('paddle',function(dat){
-				    game.physics.setTouchPoints(0,dat);
-				});
-			    io.sockets.socket(game.c1).on('paddle',function(dat){    
-				    game.physics.setTouchPoints(1,dat);
-				});
-			    setupPhysics(game);
-			}
-		    },15*1000);
-	    });
+        chooseControllers(controllers,function(a,b){
+                game.c0 = a;
+                game.c1 = b;
+                console.log(a);
+                console.log(b);
+                io.sockets.socket(game.c0).emit('nominate',0);
+                io.sockets.socket(game.c1).emit('nominate',1);
+                io.sockets.in(game.all_sockets).emit('time',15);
+                setTimeout(function(){
+                        if(game.running){
+                            game.physics.reset();
+                            io.sockets.in(game.all_sockets).emit('start',game.physics.getState());
+                            io.sockets.socket(game.c0).on('paddle',function(dat){
+                                    game.physics.setTouchPoints(0,dat);
+                                });
+                            io.sockets.socket(game.c1).on('paddle',function(dat){    
+                                    game.physics.setTouchPoints(1,dat);
+                                });
+                            io.sockets.socket(game.c0).on('dbl',function(){
+                                    game.physics.doubleTouch(0);
+                                });
+                            io.sockets.socket(game.c1).on('dbl',function(dat){    
+                                    game.physics.doubleTouch(1);
+                                });
+                            setupPhysics(game);
+                        }
+                    },15*1000);
+            });
     })(this);
     this.running = true;
 };
@@ -150,21 +164,21 @@ function chooseControllers(set,cont){
     // It turns out that writing the comparison predicate for the data is just a pain in the ass. Randomly schedule, for now.
     /*  var keys = set.map(function(i){return redisKeys[i];});
     redis.mget(keys,function(err,res){
-	    if(err){
-		console.log("Redis error, with keys :"+keys);
-		cont(set[0],set[1]);//Just choose the first two...
-	    }else{
-		res = res.map(function(obj,index){return {obj:obj,ctrl:set[index]};});
-		res.sort(function(a,b){
-			//Some predicate for the epicness.
+            if(err){
+                console.log("Redis error, with keys :"+keys);
+                cont(set[0],set[1]);//Just choose the first two...
+            }else{
+                res = res.map(function(obj,index){return {obj:obj,ctrl:set[index]};});
+                res.sort(function(a,b){
+                        //Some predicate for the epicness.
 
 
 
-			return Math.random()-0.5;
-		    });
-		cont(res[0].ctrl,res[1].ctrl);
-	    }
-	});
+                        return Math.random()-0.5;
+                    });
+                cont(res[0].ctrl,res[1].ctrl);
+            }
+        });
     */
     set.sort(function(a,b){return Math.random()-0.5;});
     cont(set[0],set[1]);
@@ -172,29 +186,29 @@ function chooseControllers(set,cont){
 // Sets up the game's physics loops
 function setupPhysics(game){
     var loop = function(){
-	// Needs some logic to terminate, however
-	if(game.physics.over())
-	    game.end();
-	if(game.running){
-	    // Update the physics here
-	    var state = game.physics.step(1/30);
-	    io.sockets.in(game.all_sockets).volatile.emit('update',state);
-	    setTimeout(loop,1/30)
-	}
+        // Needs some logic to terminate, however
+        if(game.physics.over())
+            game.end();
+        if(game.running){
+            // Update the physics here
+            var state = game.physics.step(1/30);
+            io.sockets.in(game.all_sockets).volatile.emit('update',state);
+            setTimeout(loop,1/30)
+        }
     };
     loop();
 }
 // Update the game room data on redis every n seconds.
 setInterval(function(){
-	var rooms = {};
-	for(var i in games){
-	    i = games[i];
-	    rooms[i.gameId] = JSON.stringify(i.redisRecord());
-	}
-	redis.hmset('rooms',rooms,function(err,_){
-		if(err)
-		    console.log('Redis-error:'+err);
-	    });
+        var rooms = {};
+        for(var i in games){
+            i = games[i];
+            rooms[i.gameId] = JSON.stringify(i.redisRecord());
+        }
+        redis.hmset('rooms',rooms,function(err,_){
+                if(err)
+                    console.log('Redis-error:'+err);
+            });
     },5000);
 
 
@@ -202,9 +216,9 @@ setInterval(function(){
 
 //This is the server's real main-loop. Checks if games can be started, and then starts them
 setInterval(function(){
-	for(var i in games){
-	    i = games[i];
-	    if(!i.running && i.controllers > 1)
-		i.begin();
-	}
+        for(var i in games){
+            i = games[i];
+            if(!i.running && i.controllers > 1)
+                i.begin();
+        }
     },250);
